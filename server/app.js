5e5a6dceb9bd6a6417196bae01ccefc2a90835f3
Jishna Thadichi <jishnajayan@gmail.com>
2025-02-23 18:20:57 -0800

    Move everything to a server directory
    This is to help add frontend code to client/ directory later

--- server/app.js
+++ server/app.js
@@ -2,14 +2,20 @@ require('dotenv').config()
 require('express-async-errors');
 const express = require('express');
 const connectDB = require('./db/connect');
+const path = require('path');
 
 const app = express();
 
 const authRouter = require('./routes/auth');
 
+app.use(express.static(path.join(__dirname, '../client/build')));
 app.use(express.json());
 
 app.use('/api/v1/auth', authRouter);
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
 
 // TODO: Add more sophisticated error handling. For now this is okay.
 app.use((err, req, res, next) => {
